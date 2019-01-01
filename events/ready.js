8b4af712bcf86e0db5b4f60866f2f13b6a82f89d
Lhiroxx <46294631+Lhiroxx@users.noreply.github.com>
2019-01-01 16:40:22 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,2 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
 
-var prefix = ayarlar.prefix;
-
-module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("dnd");
-   var oyun = [
-        "LHİROX HER DAİM <3"
-
-
-
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setGame(oyun[random], "https://www.twitch.tv/huseyinali27");
-        }, 2 * 2500);
 }
