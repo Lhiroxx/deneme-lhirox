128d84c6bc4b9f5987e884606087042c0896d5e5
Lhiroxx <46294631+Lhiroxx@users.noreply.github.com>
2019-01-01 16:36:04 +0300

    Update sik.js

--- komutlar/sik.js
+++ komutlar/sik.js
@@ -1,31 +1,4 @@
-const Discord = require('discord.js')
 
-exports.run = (client, message, args) => {
-  let user = message.mentions.users.first();
-  if(!user) return message.channel.send('Kimi bol bol sikmek istersin...')
-  
-
-      var resim = [
-      'https://cdn.discordapp.com/attachments/481183472363241472/502450907712192512/unknown.png'];
-
-      var resimler = resim[Math.floor(Math.random() * resim.length)];
-  
-    const sik = new Discord.RichEmbed()
-    .setAuthor(message.author.username + ` ${user.username}'ı sikti`)
-    .setColor('RANDOM')
-    .setFooter('Boşaldın mı kanka?')
-    .setImage(resimler)
-     message.channel.send(sik)
-    }
-  
-;
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
 
 exports.help = {
   name: 'sik',
