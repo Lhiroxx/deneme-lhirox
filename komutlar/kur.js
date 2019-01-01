363d07d71edcf652073fe00dc3e27359f644cf70
Lhiroxx <46294631+Lhiroxx@users.noreply.github.com>
2019-01-01 16:35:41 +0300

    Update kur.js

--- komutlar/kur.js
+++ komutlar/kur.js
@@ -1,26 +1,4 @@
-const Discord = require('discord.js');
 
-
-exports.run = (client, message, params) => {
-
-        
-        message.channel.send(':1234: **Lhirox Botu için gerekli şeyler kuruluyor...**');
-        
-        message.guild.createChannel('mod-log');
-        message.guild.createChannel('giriş-çıkış');
-        
-        message.channel.send(':white_check_mark: **Herşey Kuruldu**');
-        
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 3
-};
-
-exports.help = {
   name: 'kur',
   description: 'Bot için gerekli ayarları kurar.',
   usage: '.kur'
