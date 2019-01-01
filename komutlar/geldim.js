919efbbddce35a163bc794a45106928f30792371
Lhiroxx <46294631+Lhiroxx@users.noreply.github.com>
2019-01-01 16:38:08 +0300

    Update geldim.js

--- komutlar/geldim.js
+++ komutlar/geldim.js
@@ -1,27 +1,3 @@
-const Discord = require('discord.js');
-exports.run = function(client, message, args) {
-  const embed = new Discord.RichEmbed()
-  .setTitle("Afk Modu! \n")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor("RANDOM")
-  .addField('Şu Kullanıcı Artık Afk Değil! =>', message.author.username + '#' + message.author.discriminator)
-  .setFooter('Şakir', client.user.avatarURL)
-  message.delete();
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-
-  message.channel.send({embed});
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
 
 exports.help = {
   name: 'geldim',
